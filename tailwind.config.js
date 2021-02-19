3d62c4b7fafd135421385b754f69218db2e80181
Felix Yeung <19355619+felixyeungdev@users.noreply.github.com>
2021-02-19 18:10:34 +0800

    Wiggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,13 @@ module.exports = {
                         animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
                     },
                 },
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-3deg)" },
+                    "50%": { transform: "rotate(3deg)" },
+                },
+            },
+            animation: {
+                wiggle: "wiggle 1s ease-in-out infinite",
             },
         },
     },
